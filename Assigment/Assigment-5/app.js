113ca314c01246ba8c2ef7ab5c771c6334393236
Syed Zohaib <125762117+syedzohaib-dev@users.noreply.github.com>
2024-03-27 15:52:57 +0500

    Update app.js

--- Assigment/Assigment-5/app.js
+++ Assigment/Assigment-5/app.js
@@ -280,6 +280,188 @@ var c = 12
 if (c++ === 13)    {
     alert("condition-1 is true")
 }
+if (c === 13) {
+    alert("condition 2 is true")
+}
+if (++c < 14) {
+    alert("condition 3 is true")
+}
+if (c === 14) {
+    alert("condition 4 is true")
+}
+
+var materialCost = 20000;
+var laborCost = 2000;
+var totalCost = materialCost + laborCost;
+if (totalCost === laborCost + materialCost) {
+    alert("The cost equals");
+}
+
+
+if (true) {
+    alert("True");
+}
+if (false) {
+    alert("False");
+}
+
+if ("car" < "cat") {
+    alert("car is smaller than cat");
+}
+
+var userObtain = prompt("plz enter the obtain marks of subject 1")
+
+
+
+
+var userTotal = 300
+
+var percentage = userObtain / userTotal * 100
+
+
+if (percentage >= "80") {
+    document.write("grade: A-one" + "<br>" + "<br>")
+    document.write("Excelent" + "<br>")
+
+} else if (percentage >= "70") {
+    document.write("grade: A" + "<br>" + "<br>")
+    document.write("Good")
+
+} else if (percentage >= "60") {
+    document.write("grade: B" + "<br>" + "<br>")
+    document.write("You Need To Improve")
+}
+//  else (percentage <= "60") {
+//     document.write("FAIL " + "<br>")
+//     document.write("SORRY")
+// }
+
+
+
+document.write("Total marks is: " + userTotal + "<br>")
+document.write("Obtained marks is: " + userObtain + "<br>")
+document.write("Percentage is: " + percentage + "<br>")
+// document.write("Grade B")
+
+//  --7--
+var secretNumber = 7
+var secretUser = prompt("plz Enter your secret number")
+
+if (secretNumber == secretUser) {
+    document.write("<h1>you win " + "<br>")
+
+} else if (secretNumber == 8) {
+    document.write(" too close" + "<br>")
+}
+else {
+    document.write("Wrong answer" + "<br>")
+}
+
+
+// // --8--
+
+var userDivide = prompt("plz write and check this number is divide by 3")
+// userDivide / 3
+if (userDivide % 3 === 0) {
+    document.write("your number is divide by 3" + "<br>")
+} else {
+    document.write("not divide by 3" + "<br>")
+}
+
+// --9--
+
+var userEO = prompt("Plz enter your any number")
+if (userEO % 2 === 0) {
+    document.write("Even Number" + "<br>")
+} else {
+    document.write("ODD Number" + "<br>")
+}
+
+// --10--
+
+var outdoorTemp = prompt("plz enter the temperature")
+
+if (outdoorTemp > 40) {
+    document.write("it his to hot outsite")
+} else if (outdoorTemp > 30) {
+    document.write("The weather today is normal ")
+} else if (outdoorTemp > 20) {
+    document.write("Today weather is cool")
+} else if (outdoorTemp > 10) {
+    document.write("'OMG' today weather is to cold")
+}
+else {
+    document.write("you no" + "<br>")
+}
+
+// --11--
+
+// user Calculator
+
+var userMath1 = prompt("plz enter a first number")
+var usermath2 = prompt("plz enter your secong number")
+
+var statement = prompt("plz enter the types +  _  *  %  /")
+
+
+if (statement == "+") {
+    document.write(userMath1 + usermath2)
+} else if (statement == "-") {
+    document.write(userMath1 - usermath2)
+}
+else if (statement == "*") {
+    document.write(userMath1 * usermath2)
+}
+else if (statement == "%") {
+    document.write(userMath1 % usermath2)
+}
+else if (statement == "/") {
+    document.write(userMath1 / usermath2)
+}
+else {
+    document.write("you wrong" + "<br>")
+}
+document.write("<br>")
+
+//   ---------------------- 12-13
+
+// ---2---
+document.write("<h1>")
+var userinteger1 = prompt("plz enter first integar number")
+var userinteger2 = prompt("plz enter second integar number")
+
+if (userinteger1 > userinteger2) {
+    document.write("first integer is grater then two" + "<br>")
+} else if (userinteger2 > userinteger1) {
+    document.write("Secong integar is grater then first" + "<br>")
+} else {
+    document.write("Both are equal" + "<br>")
+}
+
+// --3--
+
+
+var userzero = prompt("plz enter the number positive or negetivve")
+
+if (userzero > 0) {
+    document.write("number is positive" + "<br>")
+}
+else if (userzero < 0) {
+    document.write("number is negetive" + "<br>")
+}
+else {
+    document.write("number is Zero" + "<br>")
+}
+
+// --4--
+
+var vovelCharacter = ("a", "e", "i", "o", "u")
+var userinter = prompt("plz enter a one charater word")
+if (userinter == vovelCharacter) {
+    document.write("Good")
+} else {
+    document.write("false")
+}
 
 
 

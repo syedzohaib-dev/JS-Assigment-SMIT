0386c3374789951cf32abe7c31ac0ed0cb731021
syed zohaib <zohaibsyed914@gmail.com>
2024-03-30 00:15:09 +0500

    add new assigment

--- Assigment/Assigment-13-15/app.js
+++ Assigment/Assigment-13-15/app.js
@@ -33,19 +33,96 @@ var per2 = studentScore[1] / totalMarks * 100
 var per3 = studentScore[2] / totalMarks * 100
 
 
-document.write("<h1>Score of ali is: " + studentScore[0] + "Percentage: " + per1)
-document.write("<h1>Score of junaid is: " + studentScore[1] + "Percentage: " + per2)
-document.write("<h1>Score of salman is: " + studentScore[2] + "Percentage: " + per3)
+document.write("<h1>Score of ali is: " + studentScore[0] + "Percentage: " + per1 + " %")
+document.write("<h1>Score of junaid is: " + studentScore[1] + "Percentage: " + per2 + " %")
+document.write("<h1>Score of salman is: " + studentScore[2] + "Percentage: " + per3 + " %<br><br>")
 
-var userinput = prompt("plz enter your favorite color")
+// --9--
 
-userinput = userinput.color[""]
+var color = ["red", "green", "blue"]
+document.write(`Original array: ${color} <br>`)
 
-var color = []
+// a
 
-// document.write("done")
+var colorAddToBeginning = prompt("Which color add to beginning to the array")
+color.unshift(colorAddToBeginning)
+document.write(`color add the begning of array: ${color} ` + `<br>`)
 
-var studentScore = [320, 230, 480, 120]
+// b
 
-document.write(studentScore[0])
+var colorAddToEnd = prompt("Which color add to end of the array")
+color.push(colorAddToEnd)
+document.write(`Color add the end of array: ${color}` + `<br>`)
+
+// c
+
+color.unshift("yellow", "blue")
+
+document.write(`Two more color add to beginning: ${color}` + `<br>`)
+
+// d
+
+color.shift()
+document.write(`array after deleting first color: ${color}` + `<br>`)
+
+// e
+
+color.pop()
+document.write(`array after deleting last color: ${color}` + `<br>`)
+
+// f
+
+var indexToAdd = parseInt(prompt("Enter the index to add the color "))
+var colorToAdd = prompt("Enter the color to add")
+color.splice(indexToAdd, 0, colorToAdd)
+document.write(`Array after adding index color: ${indexToAdd} : ${color}` + `<br>`)
+
+// g
+
+var indexToDelete = parseInt(prompt("Enter the index to delete the color"))
+var colorToDelete = parseInt(prompt("Enter the color to delete"))
+color.splice(indexToDelete, colorToDelete)
+document.write(`Array after deleting index color: ${indexToDelete} : ${color}` + `<br>`)
+
+// --10--
+
+var studentScore = [430, 220, 350, 180]
+document.write(`Score of student: ${studentScore}` + `<br>`)
+studentScore.sort()
+
+document.write(`Order score of student: ${studentScore}` + "<br>")
+
+// --11--
+
+var city = ["karachi", "lahore", "peshawer", "queta", "mardan", "islamabad"]
+document.write("City " + city + "<br>")
+
+var selectedCity = []
+
+selectedCity.push(city[0], city[1], city[2])
+
+document.write(`Selected city: ${selectedCity}` + `<br>`)
+
+// --12--
+
+var catArray = ["This", "is", "my", "cat "]
+document.write(`Array:  ${catArray}` + `<br>`)
+
+var pureString = catArray.join(" ")
+document.write(`String:  ${pureString}` + `<br>`)
+
+// --13--
+
+var device = ["Keyboard", "Mouse", "Printer", "Moniter"]
+
+
+document.write(`Device: ${device}` + `<br>`)
+
+document.write(`${device[0]}` + `<br>`)
+document.write(`${device[1]}` + `<br>`)
+document.write(`${device[2]}` + `<br>`)
+document.write(`${device[3]}` + `<br>`)
+
+// --14--
+
 

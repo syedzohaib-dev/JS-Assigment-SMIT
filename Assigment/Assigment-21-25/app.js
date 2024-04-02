b9efc92b754359e17e44f81a295dca4448c3b650
Syed Zohaib <125762117+syedzohaib-dev@users.noreply.github.com>
2024-04-02 15:52:35 +0500

    Update app.js

---
+++ Assigment/Assigment-21-25/app.js
@@ -0,0 +1,4 @@
+var firstName = prompt("Plz enter your first name");
+var lastName = prompt("Plz enter your last name");
+var fullName = (firstName + ` ` + lastName);
+document.write(`<h1>` + `${fullName}`)

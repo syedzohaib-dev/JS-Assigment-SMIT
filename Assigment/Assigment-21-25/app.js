9167f98ada4d69aa23f73699802c9e053265e460
Syed Zohaib <125762117+syedzohaib-dev@users.noreply.github.com>
2024-04-03 15:52:52 +0500

    Update app.js

--- Assigment/Assigment-21-25/app.js
+++ Assigment/Assigment-21-25/app.js
@@ -42,3 +42,89 @@ document.write(`Character index 3 ${indexNumber}<br>`)
 var newName = firstName.concat(lastName)
 document.write(`Fullname using concate method ${newName}<br>`)
 
+// --7--
+
+var cityReplace = "Hayderabad"
+var replace = cityReplace.replace("Hayder", "Islam")
+
+document.write(`City ${cityReplace}<br>`)
+document.write(`After replacement ${replace}<br>`)
+
+// --8--
+
+var message = ("Ali and Sami are best friends. They play cricket and football together.");
+var changes = message.replace(/and/g, "&")
+console.log(changes)
+document.write("Before message " + message + "<br>")
+document.write("After message " + changes + "<br>")
+
+
+// --9--
+
+var value = "840"
+var convertToNumber = parseInt(value)
+
+document.write(`value: ${value}<br>`)
+document.write(`Type: ${typeof (value)}<br>`)
+document.write(`value: ${convertToNumber}<br>`)
+document.write(`Type: ${typeof (convertToNumber)}<br>`)
+
+// --10--
+
+var smallInput = prompt("Plz enter your item")
+var capital = smallInput.toUpperCase()
+
+document.write("User input " + smallInput + "<br>")
+document.write("Upper case " + capital + "<br>")
+
+// --11--
+
+var titleInput = prompt("plz enter a word for title")
+var titleAnswer = titleInput.charAt(0).toUpperCase() + titleInput.substr(1).toLowerCase();
+console.log(titleAnswer)
+document.write(`User Input ${titleInput}<br>`)
+document.write(`Title case ${titleAnswer}<br>`)
+
+// --12--
+
+var number = 12.43
+var numToStr = number.toString().replace(".", "")
+console.log(numToStr)
+document.write(`Number ${number}<br>`)
+document.write(`Result ${numToStr}<br>`)
+
+// --13--
+
+var specialUser = prompt("Plz enter username")
+var specialSymbol = ["!", ",", ".", "@"]
+var checkCondition = false
+
+for (var i = 0; i < specialSymbol.length; i++) {
+    if (specialUser.includes(specialSymbol[i])) {
+        alert("incorrect username")
+        checkCondition = true
+        break;
+    }
+}
+if (checkCondition === false) {
+    alert, ("correct")
+}
+
+// --14--
+
+var bakery = ['cake', 'apple', 'pie', 'cookie', 'chips', 'patties']
+var userInter = prompt("Wellcome to our bakery what do you want")
+
+var checkItem = false
+
+for (var i = 0; i <= bakery.length; i++) {
+    if (userInter.toLowerCase() == bakery[i]) {
+        document.write(userInter + ' is available at index ' + bakery.indexOf(userInter))
+        checkItem = true
+    }
+}
+if (checkItem === false) {
+    document.write(`We are sorry ${userInter} is not available`)
+}
+
+// --15--
